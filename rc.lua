fa827b8bce6e35ebf9de27944e1ac93f55d7535d
Doron Behar <doron.behar@gmail.com>
2017-01-27 13:52:17 +0200

    Delete firefox rule.

--- rc.lua
+++ rc.lua
@@ -678,17 +678,7 @@ awful.rules.rules = {
 		properties = {
 			titlebars_enabled = true
 		}
-	},
-	-- Set Firefox to always map on the tag named "2" on screen 1.
-	{
-		rule = {
-			class = "Firefox"
-		},
-		properties = {
-			screen = 1,
-			tag = "2"
-		}
-	},
+	}
 }
 -- }}}
 
