8e650a9ce8f8e29ccbdb5f5c357938b98374beaf
Doron Behar <doron.behar@gmail.com>
2017-09-10 20:50:46 +0300

    `util.lua`: function `move_tag_to_screen(incr)`
    Make sure there's a tag left focused when leaving a screen. Move the
    focus to the tag most close to the one being moved.

--- util.lua
+++ util.lua
@@ -7,6 +7,17 @@ local util = {}
 function util.move_tag_to_screen(incr)
 	local s = awful.screen.focused()
 	local t = s.selected_tag
+	-- the variable `next_tag_to_be_focused` holds the object of the tag next to the tag we want to move
+	-- and it is the tag that should be focused once the move has been made.
+	local next_tag_to_be_focused = {}
+	if t.index == 1 then
+		next_tag_to_be_focused = s.tags[gears.math.cycle(#s.tags, t.index + 1)]
+	elseif t.index == #s.tags then
+		next_tag_to_be_focused = s.tags[gears.math.cycle(#s.tags, t.index - 1)]
+	else
+		next_tag_to_be_focused = s.tags[gears.math.cycle(#s.tags, t.index + incr)]
+	end
+	next_tag_to_be_focused:view_only()
 	t.screen = gears.math.cycle(screen:count(), s.index + incr)
 	awful.screen.focus_relative(incr)
 	t:view_only()
