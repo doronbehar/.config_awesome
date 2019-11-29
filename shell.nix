14531aecf57d9d536663731b8588c71e24dfb055
Doron Behar <doron.behar@gmail.com>
2019-11-29 13:08:14 +0200

    Switch to luajit in shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 pkgs.mkShell {
   GI_TYPELIB_PATH = "${playerctl}/lib/girepository-1.0";
   buildInputs = [
-    (lua53Packages.lua.withPackages(ps: with ps; [
+    (luajitPackages.lua.withPackages(ps: with ps; [
       lgi
     ]))
   ];
